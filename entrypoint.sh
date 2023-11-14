6193731f03c890c19e9beaa2bc2e957ca5000bb8
Code Mountains <thecodemountains@gmail.com>
2023-11-13 23:30:33 -0600

    update bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,9 +14,9 @@ if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
     exit 0
 fi
 
-event_type = $(jq --raw-output .action $GITHUB_EVENT_PATH)
+event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
 
-if [ $event_type != "closed" ]; then 
+if [ "$event_type" != "closed" ]; then 
     echo "::debug::The event type was '$event_type'"
     exit 0
 fi
